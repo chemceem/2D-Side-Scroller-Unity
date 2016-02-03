fc088afc3711c75df33e39f822bb518b6b5dc2ff
chemceem <chemceem@gmail.com>
2016-02-03 02:39:13 -0500

    added background image and made it to scroll during the game

---
+++ Assets/_Scripts/SpaceController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceController : MonoBehaviour {
+
+	//PRIVATE VARIABLES
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+	//PUBLIC VARIABLES
+	float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		this._transform = gameObject.GetComponent<Transform> ();
+		this.Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this._currentPosition = this._transform.position;
+		this._currentPosition -= new Vector2 (speed, 0);
+		this._transform.position = this._currentPosition;
+
+		if (this._currentPosition.x <= -456) {
+			Reset ();
+		}		
+	}
+
+	void Reset(){
+		this._transform.position = new Vector2 (456f, 0);
+	}
+}
