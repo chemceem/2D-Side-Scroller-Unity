f941ae85ab98fbcd04e1f974e8b554be112385f0
chemceem <chemceem@gmail.com>
2016-02-03 19:16:51 -0500

    added movements to the ship, added a text layer to display remaining lives

--- Assets/_Scripts/ShipController.cs
+++ Assets/_Scripts/ShipController.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class ShipController : MonoBehaviour {
 
+	//PRIVATE INSTANCE VARIABLES
+	private Transform _transform;
+	private Vector2 _currentPosition;
+	private float _playerInput;
+
+	//PUBLIC INSTANCE VARIABLES
+	public float speed = 8f;
+
 	// Use this for initialization
 	void Start () {
-	
+		this._transform = gameObject.GetComponent<Transform> ();
+		this._transform.position = new Vector2 (-520f, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this._currentPosition = this._transform.position;
+		this._playerInput = Input.GetAxis ("Vertical");
+
+		if (this._playerInput > 0) {
+			this._currentPosition += new Vector2(0, this.speed);
+		}
+
+		if (this._playerInput < 0) {
+			this._currentPosition -= new Vector2 (0, this.speed);
+		}
+		this.Checkbound ();
+		this._transform.position = this._currentPosition;
+	}
+
+	private void Checkbound() {
+		if (this._currentPosition.y < -348) {
+			this._currentPosition.y = -348;
+		}
+
+		if(this._currentPosition.y > 348) {
+			this._currentPosition.y = 348;
+		}
 	}
 }
