7648ce984e69bf890ddab8f480b94f35142c8869
chemceem <chemceem@gmail.com>
2016-02-04 21:46:01 -0500

    added collision detection among game objects

---
+++ Assets/_Scripts/SpaceshipCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Star")) {
+			Debug.Log ("collided with star");
+		}
+
+		if (other.gameObject.CompareTag ("Asteroid")) {
+			Debug.Log ("collided with asteroid");
+		}
+	}
+}
